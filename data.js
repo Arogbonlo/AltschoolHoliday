afd5748564212471c7b186f7115120477a6c02e2
Arogbonlo <arogbonloi@gmail.com>
2022-08-01 17:43:02 +0100

    # added data.js and test_dashboard.html

---
+++ data.js
@@ -0,0 +1,48 @@
+var Students = [
+
+    {
+        "firstname": "fstud1",
+        "lastname": "lstud1",
+        "track": "frontend",
+        "age": 25,
+        "gender": "male"
+    },
+
+    {
+        "firstname": "fstud2",
+        "lastname": "lstud2",
+        "track": "backend",
+        "age": 22,
+        "gender": "female"
+    },
+    {
+        "firstname": "fstud3",
+        "lastname": "lstud3",
+        "track": "cloud",
+        "age": 27,
+        "gender": "female"
+    },
+
+
+];
+
+
+var Instructors = [{
+        "name": "Bolu",
+        "courses": ["html", "js", "css"],
+        "track": "frontend"
+    },
+
+    {
+        "name": "Jacob",
+        "courses": ["aws", "linux"],
+        "track": "cloud"
+    },
+
+    {
+        "name": "Isaac",
+        "courses": ["Go", "Node"],
+        "track": "backend"
+    }
+
+]
